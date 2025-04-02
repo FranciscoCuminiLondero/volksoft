b4b382fd4ba74c414c871a17257a63338fd40126
FranciscoCumini <cuminifrancisco@gmail.com>
2025-04-01 23:19:23 -0300

    changed home name

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
+import Layout from './pages/Layout';
 
 function App() {
   return (
     <>
       <div className="relative flex flex-col min-h-screen text-white bg-gradient-to-br from-black via-gray-900 to-purple-950">
         <div className="absolute inset-0 bg-[url('https://www.transparenttextures.com/patterns/sandpaper.png')] bg-repeat opacity-10"></div>
-        {/* Header: Barra de navegación */}
-        <Header />
 
         {/* Main: Contenido de la página */}
-        <main className="flex-grow z-100">
-          <Home />
+        <main className="flex-grow  z-100">
+          <Layout />
         </main>
-
-        {/* Footer: Pie de página */}
-        <Footer />
       </div>
     </>
   );
