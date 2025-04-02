0b4531f493608ebb86fe7f0bc93230e3ec12ef34
FranciscoCumini <cuminifrancisco@gmail.com>
2025-04-02 12:53:48 -0300

    integrate i18n for translation support

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,52 +1,56 @@
-// src/components/Footer.jsx
-import React from 'react';
 import { Row, Col, Container } from 'reactstrap';
 import { FaLinkedin, FaFacebookSquare, FaEnvelope } from 'react-icons/fa';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+  const { t } = useTranslation();
   return (
     <footer className="backdrop-filter backdrop-blur-lg bg-black/30 px-10 text-white p-5 z-100">
       <Container fluid>
-        <Row className="flex flex-col md:flex-row justify-between md:items-start px-10">
-          <Col className="mb-6 md:mb-0 text-center md:text-left flex flex-col items-center">
+        <Row className="flex flex-col md:flex-row justify-between md:items-start p-x-10">
+          <div className=" text-center md:text-left flex flex-col items-center">
             <img
               src="/assets/logotipo-volksoft-light.png"
               alt="Logo Volksoft"
-              className="mx-auto md:mx-0 mb-4 opacity-50 w-25"
+              className="mx-auto mb-2 opacity-50 max-w-20"
             />
-            <p className="text-sm">
-              &copy; {new Date().getFullYear()} Volksoft. All rights reserved.
+            <p className="text-sm ml-2">
+              &copy; {new Date().getFullYear()} {t('footer.copyright')}
             </p>
-          </Col>
+          </div>
 
-          <Col className="mb-6 md:mb-0 text-center md:text-left">
-            <h4 className="text-lg font-bold mb-4">Navigation</h4>
-            <ul className="list-none p-0 space-y-1 ">
+          <Col className="flex flex-col items-center  mb-6 md:mb-0 text-center md:text-left">
+            <h4 className="text-lg font-bold mb-4">
+              {t('footer.navigation.title')}
+            </h4>
+            <ul className=" flex list-none p-0 space-x-4 ">
               <li>
                 <a href="#" className="text-white hover:text-gray-400">
-                  Home
+                  {t('footer.navigation.home')}
                 </a>
               </li>
               <li>
                 <a href="#" className="text-white hover:text-gray-400">
-                  Services
+                  {t('footer.navigation.services')}
                 </a>
               </li>
               <li>
                 <a href="#" className="text-white hover:text-gray-400">
-                  Portfolio
+                  {t('footer.navigation.portfolio')}
                 </a>
               </li>
               <li>
                 <a href="#" className="text-white hover:text-gray-400">
-                  Contact
+                  {t('footer.navigation.contact')}
                 </a>
               </li>
             </ul>
           </Col>
 
           <Col className="mb-6 md:mb-0 text-center md:text-center">
-            <h4 className="text-lg font-bold mb-4">Follow us</h4>
+            <h4 className="text-lg font-bold mb-4">
+              {t('footer.social-media.title')}
+            </h4>
             <ul className="flex justify-center md:justify-start space-x-4">
               <li>
                 <a
