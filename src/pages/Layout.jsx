b2af061f2ce96816486aad77ea1e78cf23bf7a8b
FranciscoCumini <cuminifrancisco@gmail.com>
2025-04-02 12:37:57 -0300

    add SocialSidebar component to main content

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import SocialSidebar from '../components/SocialSidebar';
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow">
+      <main className="flex-grow relative">
+        <SocialSidebar />
         <div className="container mx-auto px-4 max-w-7xl">
           <Outlet />
         </div>
