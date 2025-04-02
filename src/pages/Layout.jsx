909e01c9650779a6d8af49dc1b6e5b9100d5f29c
FranciscoCumini <cuminifrancisco@gmail.com>
2025-04-01 23:41:55 -0300

    modified innecesary class

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -6,11 +6,11 @@ import Footer from '../components/Footer';
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header className="z-50" />
+      <Header />
       <main className="flex-grow">
         <Outlet />
       </main>
-      <Footer className="sticky bottom-0" />
+      <Footer />
     </div>
   );
 };
