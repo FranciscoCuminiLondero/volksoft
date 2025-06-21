c4aaa4a69303b26ce00bbdbc250718b47f896f96
FranciscoCumini <cuminifrancisco@gmail.com>
2025-06-21 14:32:59 -0300

    feat(Home) Add About component in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Hero from '../sections/Hero';
 import Services from '../sections/Services';
-import Portfolio from '../sections/Portfolio';
+import About from '../sections/About';
 import Calendar from '../sections/Calendar';
 
 const Home = () => {
@@ -9,7 +9,8 @@ const Home = () => {
     <div className="flex flex-col gap-14">
       <Hero />
       <Services />
-      <Portfolio />
+      <About />
+      {/* <Portfolio /> */}
       <Calendar />
     </div>
   );
